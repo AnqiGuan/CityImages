93f708d69d4afbf369acf9cb887165f4ab1fd7c3
Anqi Guan <63136314+AnqiGuan@users.noreply.github.com>
2023-09-17 22:09:51 -0700

    Create an app for City Views

--- CityImages/ViewController.swift
+++ CityImages/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "defaultImg")
     }
-
-
+    
+    @IBAction func seattleImage(_ sender: Any) {
+        imageView.image = UIImage(named: "SpaceNeedle")
+    }
+    
+    @IBAction func laxImage(_ sender: Any) {
+        imageView.image = UIImage(named: "Lax")
+    }
+    
+    @IBAction func austinImage(_ sender: Any) {
+        imageView.image = UIImage(named: "Austin")
+    }
+    
+    @IBAction func newYorkImage(_ sender: Any) {
+        imageView.image = UIImage(named: "New York")
+    }
+    
+   
+    @IBAction func lasVegasImage(_ sender: Any) {
+        imageView.image = UIImage(named: "Las Vegas")
+    }
+    
+    
 }
 
